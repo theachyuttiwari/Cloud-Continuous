b3140c9ef548ef8c3834cff4789b51b8b666ac6a
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-03-24 01:03:53 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,44 @@
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+fake_movie_db = [
+    {
+        'name': 'Star Wars: Episode IX - The Rise of Skywalker',
+        'plot': 'The surviving members of the resistance face the First Order once again.',
+        'genres': ['Action', 'Adventure', 'Fantasy'],
+        'casts': ['Daisy Ridley', 'Adam Driver']
+    }
+]
 
-@app.get('/')
+class Movie(BaseModel):
+    name: str
+    plot: str
+    genres: List[str]
+    casts: List[str]
+
+
+@app.get('/', response_model=List[Movie])
 async def index():
-    return {"Real": "Python"}
+    return fake_movie_db
+
+
+# POST
+
+@app.post('/', status_code=201)
+async def add_movie(payload: Movie):
+    movie = payload.dict()
+    fake_movie_db.append(movie)
+    return {'id': len(fake_movie_db) - 1}
+
+
+@app.put('/{id}')
+async def update_movie(id: int, payload: Movie):
+    movie = payload.dict()
+    movies_length = len(fake_movie_db)
+    if 0 <= id <= movies_length:
+        fake_movie_db[id] = movie
+        return None
+    raise HTTPException(status_code=404, detail="Movie with given id not found")
